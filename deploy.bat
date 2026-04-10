893b8a2b8bd2c025b427f7ad5eadacb33956ab79
Tyler <108243683+M1dnight-ofcl@users.noreply.github.com>
2026-04-10 04:28:48 -0400

    quick checkpoint

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
 @echo off
 cls
+set t0=%time: =0%
 echo ---
 echo Starting Build...
 echo =^> pnpm i
@@ -22,4 +23,21 @@ call pnpm vercel --prod
 echo.
 echo.
 echo ---
+
+set t=%time: =0%
+set /a h=1%t0:~0,2%-100
+set /a m=1%t0:~3,2%-100
+set /a s=1%t0:~6,2%-100
+set /a c=1%t0:~9,2%-100
+set /a starttime = %h% * 360000 + %m% * 6000 + 100 * %s% + %c%
+set /a h=1%t:~0,2%-100
+set /a m=1%t:~3,2%-100
+set /a s=1%t:~6,2%-100
+set /a c=1%t:~9,2%-100
+set /a endtime = %h% * 360000 + %m% * 6000 + 100 * %s% + %c%
+set /a runtime = %endtime% - %starttime%
+set runtime = %s%.%c%
 echo Finished!
+echo Started at %t0%
+echo Ran for %runtime%0 ms
+pause
