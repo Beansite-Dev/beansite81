691cfdb63131f69d192194459334469250c61710
Tyler <m1dnightgmrofficial@gmail.com>
2026-08-07 09:56:44 -0400

    v0.71.10091 - added all vercel function and replimented olds games

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import { HelmetProvider } from 'react-helmet-async';
 import { DeclarativeRouter } from './router.tsx';
 import "./index.scss";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 const Wrapper=({}):ReactElement=><StrictMode><HelmetProvider>
   <Analytics/>
+  <SpeedInsights/>
   <DeclarativeRouter/>
 </HelmetProvider></StrictMode>;
 createRoot(document.getElementById('root')!).render(<Wrapper/>,);
